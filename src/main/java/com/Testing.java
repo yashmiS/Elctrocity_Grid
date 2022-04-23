43f990d176310b92cad54f3abda44fb85ecbf81d
RoshanIT20210274 <it20210274@my.sliit.lk>
2022-04-23 17:55:13 +0530

    Initial Commit

---
+++ src/main/java/com/Testing.java
@@ -0,0 +1,16 @@
+package com;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/testing")
+public class Testing {
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello World";
+	}
+}
